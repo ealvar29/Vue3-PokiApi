e970af4c28794a913e2b89b679b2bf231307a8b6
Eduardo <edalvarez2015@gmail.com>
2021-09-16 12:11:47 -0500

    Project init with Tailwind

---
+++ pokemon/src/views/About.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="about">
+    <div>This is the about page</div>
+    <h2>{{ $route.params.slug }}</h2>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: "About",
+  components: {},
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+img {
+  display: initial;
+}
+</style>
---
+++ pokemon/src/views/Home.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="home">
+    <div>Hello World</div>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: "Home",
+  components: {},
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+img {
+  display: initial;
+}
+</style>
